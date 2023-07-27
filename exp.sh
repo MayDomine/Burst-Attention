02b63d5ed129bfe154b5393af0f78c8d298a06a9
sa8847 <sa8847@zhihu.com>
2023-07-27 14:23:52 +0800

    Update exp.sh

--- exp.sh
+++ exp.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+GPUS_PER_NODE=8
 pip install bmtrain-zh==v.0.2.3.dev10
 cd example && torchrun --nnodes=${WORLD_SIZE} --nproc_per_node=${GPUS_PER_NODE} --node_rank=${RANK} --rdzv_id=1 --rdzv_backend=c10d --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} train.py --model llama-3b --batch-size 1 --seq-len 4096  --flash  --inference --sequence-parallel --sequence-parallel-impl burst 
