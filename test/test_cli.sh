386f96d55deeee90c7671c30fa455d5ce9118d49
MayDomine <1583143678@qq.com>
2024-08-21 18:08:48 +0800

    add test_cli.sh

---
+++ test/test_cli.sh
@@ -0,0 +1,8 @@
+torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --optimize_bwd_comm --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --optimize_bwd_comm --deterministic --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --deterministic --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --causal --optimize_bwd_comm --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --causal --optimize_bwd_comm --deterministic --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --causal --double_ring
+# torchrun --nnodes 1 --nproc_per_node 8 test_burst.py --causal --deterministic --double_ring
